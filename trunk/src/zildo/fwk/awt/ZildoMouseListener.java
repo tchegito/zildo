ea2d10fee4e42b11d770770e74ff54e15609ade9
tchegito <tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2009-12-13 19:56:55 +0000

    -add mouse move detection for ZEditor

---
+++ trunk/src/zildo/fwk/awt/ZildoMouseListener.java
@@ -0,0 +1,71 @@
+package zildo.fwk.awt;
+
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import zeditor.windows.managers.MasterFrameManager;
+
+public class ZildoMouseListener implements MouseListener, MouseMotionListener {
+
+	ZildoScrollablePanel panel;
+	
+	public ZildoMouseListener(ZildoScrollablePanel p_panel) {
+		panel=p_panel;
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent mouseevent) {
+		// Drop selected tile on map
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent mouseevent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent mouseevent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent mouseevent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent mouseevent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent mouseevent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent mouseevent) {
+		// TODO Auto-generated method stub
+		Point p=mouseevent.getPoint();
+		Point camera=panel.getPosition();
+		p.x+=camera.x;
+		p.y+=camera.y;
+		StringBuilder message=new StringBuilder();
+		message.append("X: ");
+		message.append(p.x / 16);
+		for (int i=message.length();i<16;i++) {
+			message.append(" ");
+		}
+		message.append("Y: ");
+		message.append(p.y / 16);
+		MasterFrameManager.display(message.toString(), MasterFrameManager.MESSAGE_INFO);
+	}
+}
