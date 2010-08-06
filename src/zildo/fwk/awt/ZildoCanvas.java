b5d4f03bba771fc030f464d22e8ffcd9ea659a79
tchegito <tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2010-08-06 08:25:20 +0000

    -add right click (clear map) in ZEditor

--- src/zildo/fwk/awt/ZildoCanvas.java
+++ src/zildo/fwk/awt/ZildoCanvas.java
@@ -21,6 +21,8 @@
 package zildo.fwk.awt;
 
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.lwjgl.LWJGLException;
 
@@ -34,14 +36,14 @@ import zildo.server.MapManagement;
 
 /**
  * Interface class between ZEditor and Zildo platform.
- * 
+ *
  * @author tchegito
- * 
+ *
  */
 public class ZildoCanvas extends AWTOpenGLCanvas {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
@@ -53,37 +55,56 @@ public class ZildoCanvas extends AWTOpenGLCanvas {
 		setRenderer(renderer);
 	}
 
-	public void moveCamera(int p_cameraX, int p_cameraY) {
-
-	}
-
-	public void changeTile(int p_x, int p_y, Case c) {
-		MapManagement map = EngineZildo.mapManagement;
-		Area area = map.getCurrentMap();
-		area.set_mapcase(p_x, p_y, c);
-	}
-
 	public void applyBrush(Point p) {
 		// Get brush
 		TileSelection sel = MasterFrameManager.getTileSelection();
 		if (sel != null) {
-			int dx, dy;
-			// Apply selected brush to the map
-			Area map = EngineZildo.mapManagement.getCurrentMap();
-			for (int h = 0; h < sel.getHeight(); h++) {
-				for (int w = 0; w < sel.getWidth(); w++) {
-					int item = sel.getItem(h * sel.getWidth() + w);
-					if (item != -1) {
-						dx = p.x / 16 + w;
-						dy = p.y / 16 + h;
-						if (map.getDim_x() >= dx && map.getDim_y() > dy) {
-							// We know that this is a valid location
-							map.writemap(dx, dy, item + 256 * sel.bank);
-						}
+			drawBrush(p, sel);
+		}
+	}
+
+	private void drawBrush(Point p, TileSelection p_sel) {
+		int dx, dy;
+		// Apply selected brush to the map
+		Area map = EngineZildo.mapManagement.getCurrentMap();
+		for (int h = 0; h < p_sel.getHeight(); h++) {
+			for (int w = 0; w < p_sel.getWidth(); w++) {
+				Case item = p_sel.getItem(h * p_sel.getWidth() + w);
+				if (item != null) {
+					dx = p.x / 16 + w;
+					dy = p.y / 16 + h;
+					if (map.getDim_x() >= dx && map.getDim_y() > dy) {
+						// We know that this is a valid location
+						map.set_mapcase(dx, dy + 4, item);
 					}
 				}
 			}
 		}
+	}
+
+	/**
+	 * Clear a region of the map sized by the selected brush
+	 * @param p
+	 */
+	public void clearWithBrush(Point p) {
+		TileSelection sel = MasterFrameManager.getTileSelection();
+		Point size;
+		if (sel != null) {
+			size = new Point(sel.getWidth(), sel.getHeight());
+		} else {
+			size = new Point(1, 1);
+		}
+		List<Case> cases=new ArrayList<Case>();
+		Case empty=new Case();
+		empty.setN_banque(0);
+		empty.setN_motif(54);	// Empty in outside
+		empty.setN_banque_masque(0);
+		empty.setN_motif_masque(0);
+		for (int i=0;i<size.x*size.y;i++) {
+		    cases.add(empty);
+		}
+		TileSelection emptySel = new TileSelection(size.x, size.y, cases);
+		drawBrush(p, emptySel);
 	}
 
 	public void saveMapFile(String p_mapName) {
@@ -103,7 +124,7 @@ public class ZildoCanvas extends AWTOpenGLCanvas {
 
 	/**
 	 * Set cursor size
-	 * 
+	 *
 	 * @param x
 	 *            number of horizontal tiles
 	 * @param y
