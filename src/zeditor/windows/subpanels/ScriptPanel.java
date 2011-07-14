64b32a3c8772cadd90027d98c888d5d630d46e0b
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2011-07-14 20:34:53 +0000

    ZEditor: -enhancements on ScriptPanel (auto-adjust columns size)

--- src/zeditor/windows/subpanels/ScriptPanel.java
+++ src/zeditor/windows/subpanels/ScriptPanel.java
@@ -1,5 +1,6 @@
 package zeditor.windows.subpanels;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +14,11 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 
 import zeditor.windows.managers.MasterFrameManager;
@@ -22,126 +27,183 @@ import zildo.fwk.script.xml.ActionsElement;
 import zildo.fwk.script.xml.SceneElement;
 import zildo.server.EngineZildo;
 
+@SuppressWarnings("serial")
 public class ScriptPanel extends JPanel {
 
-    final JTable scriptList;
-    private final static String[] columnNames=new String[]{"Action", "who", "pos", "what", "value", "angle",
-	"name", "type", "delta", "fx", "speed", "text", "backward", "unblock"};
+	final JTable scriptList;
+	private final static String[] columnNames = new String[] { "Action", "who",
+			"pos", "what", "value", "angle", "name", "type", "delta", "fx",
+			"speed", "text", "backward", "unblock" };
 
-    final MasterFrameManager manager;
-    final JComboBox scriptCombo;
-    JScrollPane listScroll;
-    final List<SceneElement> scenes = EngineZildo.scriptManagement.getAdventure().getScenes();
-    SceneElement focused;
+	final MasterFrameManager manager;
+	final JComboBox scriptCombo;
+	JScrollPane listScroll;
+	final List<SceneElement> scenes = EngineZildo.scriptManagement
+			.getAdventure().getScenes();
+	SceneElement focused;
 
-    public ScriptPanel(MasterFrameManager p_manager) {
+	public ScriptPanel(MasterFrameManager p_manager) {
 
-	// Initialize the needed objects
-	focused = scenes!= null && scenes.size() > 0 ? scenes.get(0) : null;
-	scriptCombo = getCombo();
-	scriptList = getScriptList();
-	listScroll = getScrollPaneList();
-	
-	// Layout
-	BoxLayout backgroundPanelLayout = new BoxLayout(this, javax.swing.BoxLayout.Y_AXIS);
-	setLayout(backgroundPanelLayout);
-	
-	// Add components
-	add(scriptCombo);
-	add(scriptList.getTableHeader());
-	add(listScroll);
+		// Initialize the needed objects
+		focused = scenes != null && scenes.size() > 0 ? scenes.get(0) : null;
+		scriptCombo = getCombo();
+		scriptList = getScriptList();
+		listScroll = getScrollPaneList();
 
-	manager = p_manager;
-    }
+		updateList();
+		
+		// Layout
+		BoxLayout backgroundPanelLayout = new BoxLayout(this,
+				javax.swing.BoxLayout.Y_AXIS);
+		setLayout(backgroundPanelLayout);
 
-    private JComboBox getCombo() {
-	ComboBoxModel backgroundComboModel = new DefaultComboBoxModel(
-		getSceneNames());
-	JComboBox combo = new JComboBox();
-	combo.setModel(backgroundComboModel);
-	combo.setSize(339, 21);
-	combo.setMaximumSize(new java.awt.Dimension(32767, 21));
-	combo.setAction(new AbstractAction("Changer le Script", null) {
-		public void actionPerformed(ActionEvent evt) {
-		    String sceneName = scriptCombo.getSelectedItem().toString();
-		    focusNamedScene(sceneName);
-		   scriptList.setModel(new ScriptTableModel(focused.actions));
+		// Add components
+		add(scriptCombo);
+		add(scriptList.getTableHeader());
+		add(listScroll);
+
+		manager = p_manager;
+	}
+
+	private JComboBox getCombo() {
+		ComboBoxModel backgroundComboModel = new DefaultComboBoxModel(
+				getSceneNames());
+		JComboBox combo = new JComboBox();
+		combo.setModel(backgroundComboModel);
+		combo.setSize(339, 21);
+		combo.setMaximumSize(new java.awt.Dimension(32767, 21));
+		combo.setAction(new AbstractAction("Changer le Script", null) {
+			public void actionPerformed(ActionEvent evt) {
+				String sceneName = scriptCombo.getSelectedItem().toString();
+				focusNamedScene(sceneName);
+				updateList();
+			}
+		});
+		return combo;
+	}
+
+	private void focusNamedScene(String p_name) {
+		for (SceneElement scene : scenes) {
+			if (scene.id.equals(p_name)) {
+				focused = scene;
+			}
 		}
-	    });
-	return combo;
-    }
-    
-    private void focusNamedScene(String p_name) {
-	for (SceneElement scene : scenes) {
-	    if (scene.id.equals(p_name)) {
-		focused = scene;
-	    }
 	}
-    }
-    
-    private JScrollPane getScrollPaneList() {
-	JScrollPane backgroundScroll = new JScrollPane();
-	    backgroundScroll
-	    .setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-	    backgroundScroll
-	    .setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-	    backgroundScroll.setViewportView(scriptList);
-	return backgroundScroll;
-    }
-    
-    private JTable getScriptList() {
-	JTable list = new JTable(null, columnNames);
-	list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-	list.setModel(new ScriptTableModel(focused.actions));
-	
-	list.setAutoResizeMode(JTable.AUTO_RESIZE_OFF); //ALL_COLUMNS);
-	
-	int length=160;
-    	for (int i=0;i<columnNames.length;i++) {
-    	    TableColumn col = list.getColumnModel().getColumn(i);
-    	    //col.setPreferredWidth(length);
-    	    length=40;
-	}
-	return list;
-    }
 
-    /**
-     * Return the scenes names, designed for a JComboBox
-     * @return Object[]
-     */
-    private Object[] getSceneNames() {
-	List<String> sceneNames = new ArrayList();
-	for (SceneElement scene : scenes) {
-	    sceneNames.add(scene.id);
+	private JScrollPane getScrollPaneList() {
+		JScrollPane backgroundScroll = new JScrollPane();
+		backgroundScroll
+				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		backgroundScroll
+				.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		backgroundScroll.setViewportView(scriptList);
+		return backgroundScroll;
 	}
-	return sceneNames.toArray();
-    }
-    
-    public static class ScriptTableModel extends DefaultTableModel {
+
+	private JTable getScriptList() {
+		JTable list = new JTable(null, columnNames);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		return list;
+	}
+
+	private void updateList() {
+		DefaultTableModel model = new ScriptTableModel(focused.actions);
+		scriptList.setModel(model);
+
+		autoResizeColWidth(scriptList);
+	}
 	
-	public ScriptTableModel(List<ActionElement> p_actions) {
-	    super(transformObjectArray(p_actions), columnNames);
-	}
-	
-        private static Object[][] transformObjectArray(List<ActionElement> p_actions) {
-		Object[][] data=new Object[p_actions.size()][5];
-		for (int i=0;i<p_actions.size();i++) {
-			data[i]=getRow(p_actions.get(i));
+	/**
+	 * Return the scenes names, designed for a JComboBox
+	 * 
+	 * @return Object[]
+	 */
+	private Object[] getSceneNames() {
+		List<String> sceneNames = new ArrayList<String>();
+		for (SceneElement scene : scenes) {
+			sceneNames.add(scene.id);
 		}
-		return data;
-        }
-        
-        private static Object[] getRow(ActionElement p_action) {
-            if (p_action instanceof ActionsElement) {
-        	return null;
-            } else {
-		Object[] obj=new Object[columnNames.length];
-		obj[0] = p_action.kind.name();
-		for (int i=1;i<columnNames.length;i++) {
-		    obj[i] = p_action.readAttribute(columnNames[i]);
-		}
-		return obj;
-            }
+		return sceneNames.toArray();
 	}
-    }
+
+	public static class ScriptTableModel extends DefaultTableModel {
+
+		public ScriptTableModel(List<ActionElement> p_actions) {
+			super(transformObjectArray(p_actions), columnNames);
+		}
+
+		private static Object[][] transformObjectArray(
+				List<ActionElement> p_actions) {
+			Object[][] data = new Object[p_actions.size()][5];
+			for (int i = 0; i < p_actions.size(); i++) {
+				data[i] = getRow(p_actions.get(i));
+			}
+			return data;
+		}
+
+		private static Object[] getRow(ActionElement p_action) {
+			if (p_action instanceof ActionsElement) {
+				return null;
+			} else {
+				Object[] obj = new Object[columnNames.length];
+				obj[0] = p_action.kind.name();
+				for (int i = 1; i < columnNames.length; i++) {
+					obj[i] = p_action.readAttribute(columnNames[i]);
+				}
+				return obj;
+			}
+		}
+	}
+
+	/**
+	 * Auto adjust column size with content.<br/>
+	 * Found at 'http://www.pikopong.com/blog/2008/08/13/auto-resize-jtable-column-width/'.
+	 * @param table
+	 */
+	private void autoResizeColWidth(JTable table) {
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+		int margin = 5;
+
+		for (int i = 0; i < table.getColumnCount(); i++) {
+			int vColIndex = i;
+			DefaultTableColumnModel colModel = (DefaultTableColumnModel) table
+					.getColumnModel();
+			TableColumn col = colModel.getColumn(vColIndex);
+			int width = 0;
+
+			// Get width of column header
+			TableCellRenderer renderer = col.getHeaderRenderer();
+
+			if (renderer == null) {
+				renderer = table.getTableHeader().getDefaultRenderer();
+			}
+
+			Component comp = renderer.getTableCellRendererComponent(table,
+					col.getHeaderValue(), false, false, 0, 0);
+
+			width = comp.getPreferredSize().width;
+
+			// Get maximum width of column data
+			for (int r = 0; r < table.getRowCount(); r++) {
+				renderer = table.getCellRenderer(r, vColIndex);
+				comp = renderer.getTableCellRendererComponent(table,
+						table.getValueAt(r, vColIndex), false, false, r,
+						vColIndex);
+				width = Math.max(width, comp.getPreferredSize().width);
+			}
+
+			// Add margin
+			width += 2 * margin;
+
+			// Set the width
+			col.setPreferredWidth(width);
+		}
+
+		((DefaultTableCellRenderer) table.getTableHeader().getDefaultRenderer())
+				.setHorizontalAlignment(SwingConstants.LEFT);
+
+		table.getTableHeader().setReorderingAllowed(false);
+	}
 }
