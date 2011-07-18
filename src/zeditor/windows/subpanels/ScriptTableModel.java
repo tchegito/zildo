78b733a2604e71072731564edddeb7288167f905
tchegito <tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2011-07-18 16:06:01 +0000

    ZEditor: -comboBox (unfinished)

--- src/zeditor/windows/subpanels/ScriptTableModel.java
+++ src/zeditor/windows/subpanels/ScriptTableModel.java
@@ -19,6 +19,15 @@ public class ScriptTableModel extends DefaultTableModel {
 
     }
 
+    public final static int findColumnByName(String p_name) {
+	for (int i=0;i<columnNames.length;i++) {
+	    if (columnNames[i].equals(p_name)) {
+		return i;
+	    }
+	}
+	throw new RuntimeException("Unable to find the '"+p_name+"' column");
+    }
+    
     private Object[][] transformObjectArray(List<ActionElement> p_actions) {
 	Object[][] data = new Object[p_actions.size()][5];
 	for (int i = 0; i < p_actions.size(); i++) {
@@ -38,5 +47,10 @@ public class ScriptTableModel extends DefaultTableModel {
 	    }
 	    return obj;
 	}
+    }
+    
+    @Override
+    public boolean isCellEditable(int p_row, int p_column) {
+       return true;
     }
 }
