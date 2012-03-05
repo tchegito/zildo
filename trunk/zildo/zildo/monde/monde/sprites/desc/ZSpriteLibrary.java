bc1ec4c1e8a1bf41cafc8335d7842d24a516dba7
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2012-03-05 21:00:46 +0000

    Project splitting (2/3) : zildo

---
+++ trunk/zildo/zildo/monde/monde/sprites/desc/ZSpriteLibrary.java
@@ -0,0 +1,97 @@
+package zildo.monde.sprites.desc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ZSpriteLibrary {
+
+	final static List<SpriteDescription> list = new ArrayList<SpriteDescription>();
+	
+	public final static List<SpriteDescription> getList() {
+		return list;
+	}
+	
+	static {
+		// Select only posable elements (discard all in-game animations)
+		for (ElementDescription desc : ElementDescription.values()) {
+			switch (desc) {
+			case SHADOW:
+			case SHADOW_LARGE:
+			case SHADOW_MINUS:
+			case SHADOW_SMALL:
+			case LEAF2:
+			case SMOKE_SMALL:
+			case TINY_ROCK2:
+			case DEATH_ANIM1:
+			case DEATH_ANIM2:
+			case DEATH_ANIM3:
+			case DEATH_ANIM4:
+			case DEATH_ANIM5:
+			case DEATH_ANIM6:
+			case DEATH_ANIM7:
+			case HEART_LEFT:
+			case HEART_RIGHT:
+			case SPARK_LEFT:
+			case SPARK_RIGHT:
+			case SPARK_UPLEFT:
+			case SPARK_UPRIGHT:
+			case GREENMONEY2:
+			case GREENMONEY3:
+			case BLUEMONEY2:
+			case BLUEMONEY3:
+			case REDMONEY2:
+			case REDMONEY3:
+			case SMOKE_FEET1:
+			case SMOKE_FEET2:
+			case SMOKE_FEET3:
+			case ROCK_BIG2:
+			case ARROW_DOWN:
+			case ARROW_LAND_DOWN1:
+			case ARROW_LAND_DOWN2:
+			case ARROW_LAND_DOWN3:
+			case ARROW_LAND_LEFT1:
+			case ARROW_LAND_LEFT2:
+			case ARROW_LAND_LEFT3:
+			case ARROW_LAND_RIGHT1:
+			case ARROW_LAND_RIGHT2:
+			case ARROW_LAND_RIGHT3:
+			case ARROW_LAND_UP1:
+			case ARROW_LAND_UP2:
+			case ARROW_LAND_UP3:
+			case ARROW_LEFT:
+			case ARROW_RIGHT:
+			case BOOMERANG2:
+			case BOOMERANG3:
+			case BOOMERANG4:
+			case ENEMYARC_DOWN1:
+			case ENEMYARC_DOWN2:
+			case ENEMYARC_LEFT1:
+			case ENEMYARC_LEFT2:
+			case ENEMYARC_RIGHT2:
+			case ENEMYARC_UP1:
+			case ENEMYARC_UP2:
+			case IMPACT1:
+			case IMPACT2:
+			case IMPACT3:
+			case IMPACT4:
+			case REDBALL1:
+			case REDBALL2:
+			case REDBALL3:
+			case EXPLO1:
+			case EXPLO2:
+			case EXPLO3:
+			case EXPLOSMOKE1:
+			case EXPLOSMOKE2:
+			case EXPLOSMOKE3:
+			case QUAD2:	case QUAD3: case QUAD4: case QUAD5:
+			case QUAD6:	case QUAD7: case QUAD8:
+				
+				break;
+			default:
+				list.add(desc);
+			}
+		}
+		list.addAll(Arrays.asList(GearDescription.values()));
+	}
+}
