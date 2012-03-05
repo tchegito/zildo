6bd08320bbf9dbeee54c31f77aa5ca63bc212e8e
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2012-03-05 21:00:34 +0000

    Project splitting (2/3) : zildo

---
+++ zildo/zildo/fwk/fwk/gfx/GraphicStuff.java
@@ -0,0 +1,34 @@
+package zildo.fwk.gfx;
+
+import java.util.logging.Logger;
+
+import zildo.fwk.opengl.compatibility.FBO;
+import zildo.fwk.opengl.compatibility.VBO;
+import zildo.monde.util.Vector4f;
+
+public abstract class GraphicStuff {
+
+
+    public FBO fbo;
+    protected VBO vbo;
+
+    protected Logger logger = Logger.getLogger("GraphicStuff");
+
+    public void cleanFBO(int id) {
+        fbo.cleanUp(id);
+        logger.info("Deleted FBO " + id);
+    }
+
+    public void cleanDepthBuffer(int id) {
+        fbo.cleanDepthBuffer(id);
+        logger.info("Deleted depth buffer " + id);
+    }
+    
+    public abstract VBO createVBO();
+	public abstract float[] getFloat(int p_info,int p_size);
+	public abstract int generateTexture(int sizeX, int sizeY);
+	public abstract int generateDepthBuffer();
+    public abstract void cleanTexture(int id);
+    public abstract Vector4f createColor(long value);
+	public abstract void setCurrentColor(float[] p_color);
+}
