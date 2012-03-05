525006c1d20a38a4f03ea63455ef1060a5ddef34
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2012-03-05 21:25:19 +0000

    Project splitting (3/3) : zildo-platform-lwjgl

---
+++ trunk/zildo-platform-lwjgl/src/zildo/platform/opengl/LwjglPixelShaders.java
@@ -0,0 +1,102 @@
+/**
+ * Legend of Zildo
+ * Copyright (C) 2006-2012 Evariste Boussaton
+ * Based on original Zelda : link to the past (C) Nintendo 1992
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package zildo.platform.opengl;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.opengl.ARBFragmentShader;
+import org.lwjgl.opengl.ARBShaderObjects;
+import org.lwjgl.opengl.GLContext;
+import org.lwjgl.opengl.Util;
+
+import zildo.fwk.ZUtils;
+import zildo.fwk.gfx.PixelShaders;
+import zildo.monde.util.Vector4f;
+
+/**
+ * @author Tchegito
+ *
+ */
+public class LwjglPixelShaders extends PixelShaders {
+
+	@Override
+	public boolean canDoPixelShader() {
+        return GLContext.getCapabilities().GL_ARB_shader_objects &&
+        GLContext.getCapabilities().GL_ARB_fragment_shader &&
+        GLContext.getCapabilities().GL_ARB_vertex_shader &&
+        GLContext.getCapabilities().GL_ARB_shading_language_100;
+	}
+	
+	@Override
+	protected int doCreatePixelShader(ByteBuffer shaderPro) {
+		// Create pixel shader
+		int vertexShader= ARBShaderObjects.glCreateShaderObjectARB(ARBFragmentShader.GL_FRAGMENT_SHADER_ARB);
+		ARBShaderObjects.glShaderSourceARB(vertexShader, shaderPro);
+		
+		// Compile, link, validate
+		ARBShaderObjects.glCompileShaderARB(vertexShader);
+		
+		int programObject  = ARBShaderObjects.glCreateProgramObjectARB();
+		ARBShaderObjects.glAttachObjectARB(programObject, vertexShader);
+		
+		ARBShaderObjects.glLinkProgramARB(programObject);
+		ARBShaderObjects.glValidateProgramARB(programObject);
+		
+		printLogInfo(vertexShader);
+		
+		return programObject;
+	}
+	
+	
+	private void printLogInfo(int obj)
+	{
+		IntBuffer iVal = ZUtils.createIntBuffer(1);
+		ARBShaderObjects.glGetObjectParameterARB(obj, ARBShaderObjects.GL_OBJECT_INFO_LOG_LENGTH_ARB, iVal);
+ 
+		int length = iVal.get();
+		if (length > 0)
+		{
+			// We have some info we need to output.
+			ByteBuffer infoLog = ZUtils.createByteBuffer(length);
+			iVal.flip();
+			ARBShaderObjects.glGetInfoLogARB(obj,  iVal, infoLog);
+			byte[] infoBytes = new byte[length];
+			infoLog.get(infoBytes);
+			String out = new String(infoBytes);
+ 
+			System.out.println("Info log:\n"+out);
+		}
+ 
+		Util.checkGLError();
+	}
+	
+	@Override
+	protected void doSetParameter(int psId, ByteBuffer name, Vector4f color) {
+		int location = ARBShaderObjects.glGetUniformLocationARB(psId, name);
+		ARBShaderObjects.glUniform4fARB(location, color.x, color.y, color.z, color.w);
+	}
+
+	@Override
+	protected void deletePixelShader(int id) {
+		ARBShaderObjects.glDeleteObjectARB(id);
+	}
+}
---
+++ trunk/zildo-platform-lwjgl/src/zildo/platform/opengl/compatibility/VBOHardware.java
@@ -0,0 +1,95 @@
+/**
+ * Legend of Zildo
+ * Copyright (C) 2006-2012 Evariste Boussaton
+ * Based on original Zelda : link to the past (C) Nintendo 1992
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package zildo.platform.opengl.compatibility;
+
+import java.nio.IntBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.ARBVertexBufferObject;
+import org.lwjgl.opengl.GL11;
+
+import zildo.fwk.opengl.compatibility.VBOBuffers;
+import zildo.platform.opengl.GLUtils;
+
+public class VBOHardware extends VBOSoftware {
+	
+	static Map<Integer, VBOBuffers> vboId = new HashMap<Integer, VBOBuffers>();
+	
+	@Override
+	public VBOBuffers create(int p_numPoints) {
+        
+		VBOBuffers bufs=super.create(p_numPoints);
+		bufs.vertexBufferId = GLUtils.createVBO();
+		bufs.normalBufferId = GLUtils.createVBO();
+		bufs.textureBufferId = GLUtils.createVBO();
+		bufs.indiceBufferId = GLUtils.createVBO();
+        
+        return bufs;
+	}
+	
+	@Override
+	public void draw(VBOBuffers p_bufs) {
+		preDraw();
+		
+        ARBVertexBufferObject.glBindBufferARB(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB, p_bufs.vertexBufferId);
+        GL11.glVertexPointer(3, GL11.GL_FLOAT, 0, 0);
+
+        ARBVertexBufferObject.glBindBufferARB(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB, p_bufs.normalBufferId);
+        GL11.glNormalPointer(GL11.GL_FLOAT, 0, 0);
+
+        ARBVertexBufferObject.glBindBufferARB(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB, p_bufs.textureBufferId);
+        GL11.glTexCoordPointer(2, GL11.GL_FLOAT, 0, 0);	
+        
+        GL11.glDrawElements(GL11.GL_TRIANGLES, p_bufs.indices);
+
+	}
+	
+	@Override
+	public void cleanUp(VBOBuffers p_bufs) {
+        IntBuffer buf = BufferUtils.createIntBuffer(1);
+        buf.put(p_bufs.vertexBufferId);
+        buf.flip();
+        ARBVertexBufferObject.glDeleteBuffersARB(buf);
+        buf = BufferUtils.createIntBuffer(1);
+        buf.put(p_bufs.textureBufferId);
+        buf.flip();
+        ARBVertexBufferObject.glDeleteBuffersARB(buf);
+        buf = BufferUtils.createIntBuffer(1);
+        buf.put(p_bufs.normalBufferId);
+        buf.flip();
+        ARBVertexBufferObject.glDeleteBuffersARB(buf);
+        buf = BufferUtils.createIntBuffer(1);
+        buf.put(p_bufs.indiceBufferId);
+        buf.flip();
+        ARBVertexBufferObject.glDeleteBuffersARB(buf);		
+	}
+	
+	@Override
+	public void endInitialization(VBOBuffers p_bufs) {
+		super.endInitialization(p_bufs);
+        GLUtils.bufferData(p_bufs.vertexBufferId, p_bufs.vertices);
+        GLUtils.bufferData(p_bufs.normalBufferId, p_bufs.normals);
+        GLUtils.bufferData(p_bufs.textureBufferId, p_bufs.textures);
+        GLUtils.bufferData(p_bufs.indiceBufferId, p_bufs.indices);		
+	}
+}
