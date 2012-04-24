91b349c26169676e5a0912b62e846b29245de31d
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2012-04-24 21:54:27 +0000

    add a 'listFile' functionality to platform dependent plugin. (for savegames)

---
+++ trunk/zildo/src/zildo/fwk/file/ClassicFileUtil.java
@@ -0,0 +1,45 @@
+/**
+ * Legend of Zildo
+ * Copyright (C) 2006-2012 Evariste Boussaton
+ * Based on original Zelda : link to the past (C) Nintendo 1992
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package zildo.fwk.file;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import zildo.resource.Constantes;
+
+/**
+ * @author Tchegito
+ *
+ */
+public class ClassicFileUtil implements FileUtil {
+
+	@Override
+	public EasyBuffering openFile(String path) {
+		return new EasyReadingFile(path);
+	}
+
+	@Override
+	public File[] listFiles(String path, FilenameFilter filter) {
+		File startPath = new File(Constantes.DATA_PATH + path);
+		return startPath.listFiles(filter);
+	}
+
+}
