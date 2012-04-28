f1de3c0793cb71446e41af60443ef6b932265a73
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2012-04-28 21:33:11 +0000

    Fix display problems with map bottom.

--- trunk/zildo/src/zildo/monde/map/Area.java
+++ trunk/zildo/src/zildo/monde/map/Area.java
@@ -143,7 +143,7 @@ public class Area implements EasySerializable {
 		if (x < 0 || x > dim_x) {
 			return null;
 		}
-		if (y < 0 || y > dim_y) {
+		if (y < 0 || y > (dim_y + 3)) {
 			return null;
 		}
 		return mapdata[y][x];
