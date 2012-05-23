0289836cc3349e06809bf55737b18558da091cf0
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2012-05-23 19:10:42 +0000

    Working OpenGL ES 1.1 version

---
+++ tags/ANDROID_OPENGL_ES_1/zildo-platform-android/src/zildo/platform/filter/AndroidBilinearFilter.java
@@ -0,0 +1,77 @@
+/**
+ * Legend of Zildo
+ * Copyright (C) 2006-2012 Evariste Boussaton
+ * Based on original Zelda : link to the past (C) Nintendo 1992
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package zildo.platform.filter;
+
+import javax.microedition.khronos.opengles.GL11;
+
+import zildo.fwk.gfx.GraphicStuff;
+import zildo.fwk.gfx.filter.BilinearFilter;
+import zildo.platform.opengl.AndroidOpenGLGestion;
+
+
+public class AndroidBilinearFilter extends BilinearFilter {
+
+	GL11 gl11;
+	
+	public AndroidBilinearFilter(GraphicStuff graphicStuff) {
+		super(graphicStuff);
+    	gl11 = (GL11) AndroidOpenGLGestion.gl10;
+	}
+	
+	@Override
+	public boolean renderFilter() {
+		graphicStuff.fbo.endRendering();
+		
+		// Select right texture
+		gl11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
+        gl11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+        gl11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+        
+        // Disable blend
+		gl11.glDisable(GL11.GL_BLEND);
+
+		gl11.glMatrixMode(GL11.GL_MODELVIEW);
+		gl11.glLoadIdentity();
+		gl11.glMatrixMode(GL11.GL_PROJECTION);
+		gl11.glPushMatrix();
+		// Flip the image vertically
+		gl11.glTranslatef(0,sizeY,0);
+		gl11.glScalef(1, -1, 1);
+		// FIXME: was previously color3f
+		gl11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+		
+		// Draw texture with depth
+		super.render();
+
+		gl11.glPopMatrix();
+		
+		gl11.glMatrixMode(GL11.GL_MODELVIEW);
+		
+		return true;
+	}
+	
+	@Override
+	public void preFilter() {
+		graphicStuff.fbo.startRendering(fboId, sizeX, sizeY);
+		//gl11.glClear(GL11.GL_COLOR_BUFFER_BIT); // Clear The Screen And The Depth Buffer
+	}
+	
+}
