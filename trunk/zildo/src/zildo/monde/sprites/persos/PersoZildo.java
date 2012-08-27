b75cb4eba90b5328dedb9d44d31317b79b0d6802
tchegito <tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2012-08-27 12:40:47 +0000

    Issue 36 : NPE in throwSomething

--- trunk/zildo/src/zildo/monde/sprites/persos/PersoZildo.java
+++ trunk/zildo/src/zildo/monde/sprites/persos/PersoZildo.java
@@ -930,43 +930,46 @@ public class PersoZildo extends Perso {
 	public void throwSomething() {
 		// On jette un objet
 		Element element = getEn_bras();
-		element.setLinkedPerso(null);
-		setEn_bras(null);
 		setMouvement(MouvementZildo.VIDE);
-		element.setX(getX() + 1);
-		element.setY(getY());
-		element.setZ(21.0f + 1.0f);
-		element.setVx(0.0f);
-		element.setVy(0.0f);
-		element.setVz(0.0f);
-		element.setAx(0.0f);
-		element.setAy(0.0f);
-		element.setAz(-0.07f);
-		element.setLinkedPerso(this); // Declare this element thrown by Zildo
-										// (so it can't collide with him)
-		element.setAngle(angle);
-		element.flying = true;
-		element.relativeZ = EngineZildo.mapManagement.getCurrentMap().readAltitude((int) x / 16, (int) y / 16);
-
-		switch (getAngle()) {
-		case NORD:
-			element.setVy(-4.0f);
-			element.setFy(0.04f);
-			break;
-		case EST:
-			element.setVx(4.0f);
-			element.setFx(0.04f);
-			break;
-		case SUD:
-			element.setVy(4.0f);
-			element.setFy(0.04f);
-			break;
-		case OUEST:
-			element.setVx(-4.0f);
-			element.setFx(0.04f);
-			break;
+		if (element != null) {
+			// Element shouldn't be null, but it happened !
+			setEn_bras(null);
+			element.setLinkedPerso(null);
+			element.setX(getX() + 1);
+			element.setY(getY());
+			element.setZ(21.0f + 1.0f);
+			element.setVx(0.0f);
+			element.setVy(0.0f);
+			element.setVz(0.0f);
+			element.setAx(0.0f);
+			element.setAy(0.0f);
+			element.setAz(-0.07f);
+			element.setLinkedPerso(this); // Declare this element thrown by Zildo
+											// (so it can't collide with him)
+			element.setAngle(angle);
+			element.flying = true;
+			element.relativeZ = EngineZildo.mapManagement.getCurrentMap().readAltitude((int) x / 16, (int) y / 16);
+	
+			switch (getAngle()) {
+			case NORD:
+				element.setVy(-4.0f);
+				element.setFy(0.04f);
+				break;
+			case EST:
+				element.setVx(4.0f);
+				element.setFx(0.04f);
+				break;
+			case SUD:
+				element.setVy(4.0f);
+				element.setFy(0.04f);
+				break;
+			case OUEST:
+				element.setVx(-4.0f);
+				element.setFx(0.04f);
+				break;
+			}
+			EngineZildo.soundManagement.broadcastSound(BankSound.ZildoLance, this);
 		}
-		EngineZildo.soundManagement.broadcastSound(BankSound.ZildoLance, this);
 	}
 
 	/**
