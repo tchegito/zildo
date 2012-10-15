fd94180b2d45260acfe8a14de7386da98a40212a
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2012-10-15 17:48:22 +0000

    Fix issue on LWJGL platform

--- trunk/zildo/src/zildo/fwk/gfx/PixelShaders.java
+++ trunk/zildo/src/zildo/fwk/gfx/PixelShaders.java
@@ -72,6 +72,7 @@ public abstract class PixelShaders {
 			"uniform vec4 Color2;",
 			"uniform vec4 Color3;",
 			"uniform vec4 Color4;",
+			"uniform vec4 curColor;",
 			"uniform sampler2D tex;",
 			"void main (void) {",
 			"	vec4 texel=texture2D(tex, gl_TexCoord[0].st);",
@@ -79,7 +80,7 @@ public abstract class PixelShaders {
 			"		gl_FragColor = Color3;",
 			"	else if (ceil(texel.w*2.0) == 2.0*Color2.w && texel.xyz==Color2.xyz)",
 			"		gl_FragColor = Color4;",
-			"	else gl_FragColor=texel;",
+			"	else gl_FragColor=texel * curColor;",
 			"}"};
 /*
 			{"!!ARBfp1.0",
