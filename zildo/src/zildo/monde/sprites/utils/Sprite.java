ab8ad2498fd51fa0a5716ab285a34834de34c001
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2012-12-10 06:42:48 +0000

    Work on Zildo sprites (unfinished)

--- zildo/src/zildo/monde/sprites/utils/Sprite.java
+++ zildo/src/zildo/monde/sprites/utils/Sprite.java
@@ -22,6 +22,7 @@ package zildo.monde.sprites.utils;
 
 import zildo.fwk.bank.SpriteBank;
 import zildo.monde.sprites.Reverse;
+import zildo.monde.sprites.Rotation;
 import zildo.monde.sprites.desc.ElementDescription;
 import zildo.monde.sprites.desc.SpriteDescription;
 import zildo.monde.sprites.desc.ZildoDescription;
@@ -30,16 +31,22 @@ public class Sprite {
 
 	public SpriteDescription spr;	// Identify the sprite (Bank + sprite number)
 	public Reverse reverse;				// Reverse (horizontal and/or vertical)
+	public Rotation rotate;
 	
 	public Sprite(int p_nSpr, int p_nBank, Reverse p_reverse) {
 		switch (p_nBank) {
 		case SpriteBank.BANK_ELEMENTS:
-			spr=ElementDescription.fromInt(p_nSpr);
+			spr = ElementDescription.fromInt(p_nSpr);
 			break;
 		case SpriteBank.BANK_ZILDO:
-			spr=ZildoDescription.fromInt(p_nSpr);
+			spr = ZildoDescription.fromInt(p_nSpr);
 			break;
 		}
-		reverse=p_reverse;
+		reverse = p_reverse;
+	}
+	
+	public Sprite(int p_nSpr, int p_nBank, Reverse p_reverse, Rotation p_rotation) {
+		this(p_nSpr, p_nBank, p_reverse);
+		rotate = p_rotation;
 	}
 }
