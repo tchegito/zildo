420be6da2836f84110ac14d6a3bb7481fddd1cb6
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2013-09-16 19:57:50 +0000

    -remove boring setter/getter in Zone object

--- trunk/zildo/src/zildo/monde/util/Zone.java
+++ trunk/zildo/src/zildo/monde/util/Zone.java
@@ -27,38 +27,6 @@ public class Zone {
 	public int x1, y1;
 	public int x2, y2;
 
-	public int getX1() {
-		return x1;
-	}
-
-	public void setX1(int x1) {
-		this.x1 = x1;
-	}
-
-	public int getY1() {
-		return y1;
-	}
-
-	public void setY1(int y1) {
-		this.y1 = y1;
-	}
-
-	public int getX2() {
-		return x2;
-	}
-
-	public void setX2(int x2) {
-		this.x2 = x2;
-	}
-
-	public int getY2() {
-		return y2;
-	}
-
-	public void setY2(int y2) {
-		this.y2 = y2;
-	}
-
 	public Zone() {
 
 	}
@@ -101,7 +69,11 @@ public class Zone {
 	public boolean isCrossing(Zone p_zone) {
 		return new Rectangle(this).isCrossing(new Rectangle(p_zone));
 	}
-
+	
+	public Point getCenter() {
+		return new Point(x1 + x2 / 2, y1 + y2 / 2);
+	}
+	
 	@Override
 	public String toString() {
 		return x1 + ", " + y1 + " " + x2 + "x" + y2;
