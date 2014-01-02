21d17adf139ec419ec4a7a2cc092226d2027dc56
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2014-01-02 00:13:04 +0000

    -new materials -enhanced scroll in Zeditor (background and sprite panel)

--- trunk/zeditor/src/zeditor/tools/banque/Foret3.java
+++ trunk/zeditor/src/zeditor/tools/banque/Foret3.java
@@ -112,7 +112,6 @@ public class Foret3 extends Banque {
 				new Point(48, 224), new Point(64, 224),
 				/* Fisherman door */
 				new Point(0, 208), new Point(16, 208),
-				new Point(0, 224)	// Broken wall
 				};
 
 		pkmChanges = Arrays.asList(new GraphChange[] { new GraphChange("exteria6", 0, 0),
