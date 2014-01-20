244178c6c41342270ad9054c719d08e32d0a1d6a
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2014-01-20 19:38:23 +0000

    Create branch for McFlac

---
+++ branches/layerFeature_papy/zildo-platform-lwjgl/src/zildo/platform/filter/LwjglCloudFilter.java
@@ -0,0 +1,67 @@
+/**
+ * The Land of Alembrum
+ * Copyright (C) 2006-2013 Evariste Boussaton
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package zildo.platform.filter;
+
+import org.lwjgl.opengl.GL11;
+
+import zildo.client.ClientEngineZildo;
+import zildo.fwk.gfx.GraphicStuff;
+import zildo.fwk.gfx.filter.CloudFilter;
+import zildo.monde.sprites.Reverse;
+
+/**
+ * @author Tchegito
+ *
+ */
+public class LwjglCloudFilter extends CloudFilter {
+
+	public LwjglCloudFilter(GraphicStuff graphicStuff) {
+		super(graphicStuff);
+	}
+	
+	@Override
+	public boolean renderFilter() {
+		
+		super.startInitialization();
+		updateQuad(0, 0, u, v, Reverse.NOTHING);
+		this.endInitialization();
+		
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+    	GL11.glLoadIdentity();
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glPushMatrix();
+		GL11.glTranslatef(0,-sizeY,1);
+
+		float colorFactor=0.2f;
+		GL11.glColor4f(colorFactor, colorFactor, colorFactor, 0.1f);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_ZERO, GL11.GL_ONE_MINUS_SRC_COLOR);
+
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, ClientEngineZildo.tileEngine.texCloudId);
+		super.render();
+
+		GL11.glDisable(GL11.GL_BLEND);
+		
+		GL11.glPopMatrix();
+		
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		return true;
+	}
+}
---
+++ branches/layerFeature_papy/zildo-platform-lwjgl/src/zildo/platform/opengl/compatibility/FBOHardware.java
@@ -0,0 +1,138 @@
+/**
+ * The Land of Alembrum
+ * Copyright (C) 2006-2013 Evariste Boussaton
+ * 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package zildo.platform.opengl.compatibility;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+import org.lwjgl.opengl.EXTFramebufferObject;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GLContext;
+
+import zildo.Zildo;
+import zildo.fwk.ZUtils;
+import zildo.fwk.opengl.compatibility.FBO;
+
+/**
+ * @author tchegito
+ */
+public class FBOHardware implements FBO {
+
+	@Override
+	public int create() {
+		if (GLContext.getCapabilities().GL_EXT_framebuffer_object) {
+			IntBuffer buffer = ByteBuffer.allocateDirect(1 * 4)
+					.order(ByteOrder.nativeOrder()).asIntBuffer(); // allocate a
+																	// 1 int
+																	// byte
+			// buffer
+			EXTFramebufferObject.glGenFramebuffersEXT(buffer); // generate
+			int fboId = buffer.get();
+
+			checkCompleteness(fboId);
+
+			return fboId;
+		} else {
+			throw new RuntimeException("Unable to create FBO");
+		}
+	}
+
+	@Override
+	public void bindToTexture(int myTextureId, int myFBOId) {
+		// On bind le FBO à la texture
+		EXTFramebufferObject.glBindFramebufferEXT(
+				EXTFramebufferObject.GL_FRAMEBUFFER_EXT, myFBOId);
+		EXTFramebufferObject.glFramebufferTexture2DEXT(
+				EXTFramebufferObject.GL_FRAMEBUFFER_EXT,
+				EXTFramebufferObject.GL_COLOR_ATTACHMENT0_EXT,
+				GL11.GL_TEXTURE_2D, myTextureId, 0);
+
+		// Puis on détache la texture de la vue
+		EXTFramebufferObject.glBindFramebufferEXT(
+				EXTFramebufferObject.GL_FRAMEBUFFER_EXT, 0);
+	}
+
+	@Override
+	public void startRendering(int myFBOId, int sizeX, int sizeY) {
+		EXTFramebufferObject.glBindFramebufferEXT(
+				EXTFramebufferObject.GL_FRAMEBUFFER_EXT, myFBOId);
+
+		GL11.glPushAttrib(GL11.GL_VIEWPORT_BIT);
+		GL11.glViewport(0, 0, Zildo.viewPortX, Zildo.viewPortY);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void endRendering() {
+		EXTFramebufferObject.glBindFramebufferEXT(
+				EXTFramebufferObject.GL_FRAMEBUFFER_EXT, 0);
+		GL11.glPopAttrib();
+	}
+
+	@Override
+	public void cleanUp(int id) {
+		EXTFramebufferObject.glDeleteFramebuffersEXT(ZUtils.getBufferWithId(id));
+	}
+
+	public void checkCompleteness(int myFBOId) {
+		int framebuffer = EXTFramebufferObject
+				.glCheckFramebufferStatusEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT);
+		switch (framebuffer) {
+		case EXTFramebufferObject.GL_FRAMEBUFFER_COMPLETE_EXT:
+			break;
+		case EXTFramebufferObject.GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT_EXT:
+			throw new RuntimeException(
+					"FrameBuffer: "
+							+ myFBOId
+							+ ", has caused a GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT_EXT exception");
+		case EXTFramebufferObject.GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT_EXT:
+			throw new RuntimeException(
+					"FrameBuffer: "
+							+ myFBOId
+							+ ", has caused a GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT_EXT exception");
+		case EXTFramebufferObject.GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT:
+			throw new RuntimeException(
+					"FrameBuffer: "
+							+ myFBOId
+							+ ", has caused a GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT exception");
+		case EXTFramebufferObject.GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER_EXT:
+			throw new RuntimeException(
+					"FrameBuffer: "
+							+ myFBOId
+							+ ", has caused a GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER_EXT exception");
+		case EXTFramebufferObject.GL_FRAMEBUFFER_INCOMPLETE_FORMATS_EXT:
+			throw new RuntimeException(
+					"FrameBuffer: "
+							+ myFBOId
+							+ ", has caused a GL_FRAMEBUFFER_INCOMPLETE_FORMATS_EXT exception");
+		case EXTFramebufferObject.GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER_EXT:
+			throw new RuntimeException(
+					"FrameBuffer: "
+							+ myFBOId
+							+ ", has caused a GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER_EXT exception");
+		default:
+			throw new RuntimeException(
+					"Unexpected reply from glCheckFramebufferStatusEXT: "
+							+ framebuffer);
+		}
+	}
+}
