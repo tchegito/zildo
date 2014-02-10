3bb5de5a3a5527088a44265f2c44da3674449590
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2014-02-10 23:30:27 +0000

    -bugfix about 'mergedEntities' (this become too complicated ...) -fix trigo method: getAngleRadian

--- zildo/src/zildo/monde/Trigo.java
+++ zildo/src/zildo/monde/Trigo.java
@@ -6,13 +6,18 @@ import zildo.monde.util.Vector2f;
 public class Trigo {
 
 	public static final double PI_SUR_4 = Math.PI / 4d;
+	public static final double SQUARE_2 = Math.sqrt(2);
 	
 	public static double getAngleRadian(float ax, float ay) {
-		return getAngleRadian(1, 0, ax, ay);
+		return getAngleRadian(0, 0, ax, ay);
 	}
 	
 	public static double getAngleRadian(float ax, float ay, float bx, float by) {
-		double cosAngle = (bx - ax) / Point.distance(ax, ay, bx, by);
+		double distance = Point.distance(ax, ay, bx, by);
+		if (distance == 0) {
+			return 0;
+		}
+		double cosAngle = (bx - ax) / distance;
 		double result = Math.acos(cosAngle);
 		if (by < ay) {
 			result = -result;
