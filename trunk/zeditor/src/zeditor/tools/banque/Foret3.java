80dab0f3ed75de59bd2f7babdf150be5f7ecafae
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2014-04-01 23:12:55 +0000

    -new materials

--- trunk/zeditor/src/zeditor/tools/banque/Foret3.java
+++ trunk/zeditor/src/zeditor/tools/banque/Foret3.java
@@ -115,7 +115,8 @@ public class Foret3 extends Banque {
 				/* Cracked wall (cave) */
 				new Point(80, 192), new Point(96, 192), new Point(80, 208), new Point(96, 208),
 				/* Cracked wall (outside) */
-				new Point(80, 224), new Point(96, 224), new Point(80, 240), new Point(96, 240)
+				new Point(80, 224), new Point(96, 224), new Point(80, 240), new Point(96, 240),
+				new Point(48, 208)
 				};
 
 		pkmChanges = Arrays.asList(new GraphChange[] { new GraphChange("exteria6", 0, 0),
