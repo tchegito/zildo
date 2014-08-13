856e97577505186419e24d603ab89e8c3174b165
tchegito <tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2014-08-13 22:46:51 +0000

    new TU: about wrong direction (done ! It remains to fix in the real code)

--- zildo-test/test/junit/area/CheckDpadDirection.java
+++ zildo-test/test/junit/area/CheckDpadDirection.java
@@ -9,6 +9,7 @@ import zildo.Zildo;
 import zildo.fwk.input.CommonKeyboardHandler;
 import zildo.fwk.input.KeyboardHandler;
 import zildo.fwk.input.KeyboardInstant;
+import zildo.monde.Trigo;
 import zildo.monde.sprites.persos.Perso;
 import zildo.monde.sprites.persos.PersoZildo;
 import zildo.monde.util.Vector2f;
@@ -24,10 +25,11 @@ public class CheckDpadDirection extends EngineUT {
 	Perso zildo;
 	ClientState state;
 	KeyboardInstant instant = new KeyboardInstant();
+	KeyboardHandler fakedKbHandler;
 	
 	Vector2f CONSTANT_DIRECTION = new Vector2f(0, -Constantes.ZILDO_SPEED);
 	
-	private void init(int x, int y) {
+	private void init(int x, int y, Vector2f holdDirection) {
 		mapUtils.loadMap("d4m11");
 		EngineZildo.persoManagement.clearPersos(true);
 
@@ -43,14 +45,16 @@ public class CheckDpadDirection extends EngineUT {
 			renderFrames(1);
 		}
 		
-		KeyboardHandler fakedKbHandler = org.mockito.Mockito.mock(CommonKeyboardHandler.class);
+		fakedKbHandler = org.mockito.Mockito.mock(CommonKeyboardHandler.class);
 		Zildo.pdPlugin.kbHandler = fakedKbHandler;
-		when(fakedKbHandler.getDirection()).thenReturn(CONSTANT_DIRECTION);
+
+		when(fakedKbHandler.getDirection()).thenReturn(holdDirection);
 	}
 	
 	@Test
 	public void testStraightMovement() {
-		init(152, 101);
+		init(152, 101, CONSTANT_DIRECTION);
+
 		
 		int step = 50;
 		while (step-- >= 0) {
@@ -64,5 +68,26 @@ public class CheckDpadDirection extends EngineUT {
 		
 		Assert.assertTrue(zildo.y < 90);
 		Assert.assertTrue(zildo.x == 152);
+	}
+	
+	@Test
+	public void testRealisticMovement() {
+		// Simulation a realistic direction => UP with a little value on X axis, as it is with a real touch screen
+		init(152, 101, Trigo.vect((Math.PI / 2) - 0.01, -Constantes.ZILDO_SPEED));
+		
+		int step = 50;
+		while (step-- >= 0) {
+			// Make Zildo go forward
+			state.keys = instant;
+			instant.setKey(KeysConfiguration.PLAYERKEY_UP, true);
+			instant.update();
+
+			renderFrames(1);
+			
+			// Hero shouldn't derive more than small decimal on the left on X axis
+			Assert.assertTrue(Math.abs(zildo.getDelta().x) < 1);
+		}
+		
+		Assert.assertTrue(zildo.y < 90);
 	}
 }
