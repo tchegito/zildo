32cc131e031428022b0b9cf5ef262d424f8bf56e
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2014-10-02 21:01:20 +0000

    -fix item's wrong amount: when more than 9, and didn't decrement when hero purchased them

--- zildo/src/zildo/fwk/ZUtils.java
+++ zildo/src/zildo/fwk/ZUtils.java
@@ -149,4 +149,21 @@ public class ZUtils {
 		}
 		return sb.toString();
     }
+    
+    static byte[] buffer = new byte[10];
+    
+    /** Returns an array containing decomposition of POSITIVE value in base 10. **/
+    public static byte[] decomposeBase10(int value) {
+    	int n = value;
+    	int numDecimal = 0;
+    	while ( n > 0 || numDecimal == 0) {
+    		buffer[ numDecimal++ ] = (byte) (n % 10);
+    		n = n/10;
+    	}
+    	byte[] result = new byte[numDecimal];
+    	for (int i=0;i<numDecimal;i++) {
+    		result[i] = buffer[numDecimal - i - 1];
+    	}
+    	return result;
+    }
 }
