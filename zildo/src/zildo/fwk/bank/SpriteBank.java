c78dcd299a2ba6fbbafc6404ef7b82c1b8fbb86a
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2015-03-02 22:31:31 +0000

    -fix palette problem

--- zildo/src/zildo/fwk/bank/SpriteBank.java
+++ zildo/src/zildo/fwk/bank/SpriteBank.java
@@ -151,7 +151,7 @@ public class SpriteBank {
 	public int whichPalette(int nSpr) {
 		if (name.equals("pnj3.spr") && nSpr >= 66 ) {	// Dragon => Decroded palette
 			return 2;
-		} else if (name.equals("elem.spr") && nSpr == 243) {
+		} else if (name.equals("elem.spr") && nSpr == 245) {
 			return 2;
 		} else {
 			return 1;
