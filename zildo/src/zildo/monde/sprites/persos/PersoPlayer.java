a9a92ef93af45bcb86b9a0b29cda7b467ac59fa8
Tchegito <Tchegito@b2c6f842-1ad3-11de-8b5c-13accb87f508>
2015-04-04 15:27:21 +0000

    Fix errors due to recent refactor

--- zildo/src/zildo/monde/sprites/persos/PersoPlayer.java
+++ zildo/src/zildo/monde/sprites/persos/PersoPlayer.java
@@ -665,7 +665,7 @@ public class PersoPlayer extends Perso {
 
 			switch (angle) {
 			case NORD:
-				seqPos = computeSeq(6);
+				seqPos = computeSeq(1) % 6;
 				setNSpr(PersoDescription.PRINCESS_BUNNY.nth(11 + seqPos % 3));
 				setNBank(SpriteBank.BANK_PNJ3);
 				reverse = seqPos > 2 ? Reverse.HORIZONTAL : Reverse.NOTHING;
@@ -673,7 +673,7 @@ public class PersoPlayer extends Perso {
 				setVisible(true);
 				break;
 			case SUD:
-				seqPos = computeSeq(6);
+				seqPos = computeSeq(1) % 6;
 				setNSpr(PersoDescription.PRINCESS_BUNNY.nth(3 + seqPos % 3));
 				setNBank(SpriteBank.BANK_PNJ3);
 				reverse = seqPos > 2 ? Reverse.HORIZONTAL : Reverse.NOTHING;
@@ -685,7 +685,7 @@ public class PersoPlayer extends Perso {
 				//shiftWetFeet += 2;
 			case OUEST:
 				reverse = angle == Angle.OUEST ? Reverse.HORIZONTAL : Reverse.NOTHING;
-				setNSpr(PersoDescription.PRINCESS_BUNNY.nth(7 + computeSeq(3)));
+				setNSpr(PersoDescription.PRINCESS_BUNNY.nth(7 + computeSeq(1) % 3));
 				setNBank(SpriteBank.BANK_PNJ3);
 				shadow.setX(x+1);	// adjust shadow
 				break;
