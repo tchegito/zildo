a7907fb2304894d78b4099c69e3eeed6aad6c76d
Evariste Boussaton <tchegito@gmail.com>
2016-01-24 00:42:06 +0100

    -made spade untakeable (as it always should have been !)

--- zildo/src/zildo/monde/sprites/elements/ElementWeapon.java
+++ zildo/src/zildo/monde/sprites/elements/ElementWeapon.java
@@ -1,5 +1,7 @@
 package zildo.monde.sprites.elements;
 
+import zildo.monde.sprites.desc.ElementDescription;
+
 public class ElementWeapon extends ElementGoodies {
 
 	public ElementWeapon(int p_x, int p_y) {
@@ -7,5 +9,13 @@ public class ElementWeapon extends ElementGoodies {
 		x=p_x;
 		y=p_y;
 		volatil=false;
+	}
+	
+	@Override
+	public boolean isGoodies() {
+		if (desc != ElementDescription.SPADE) {
+			return super.isGoodies();
+		}
+		return false;
 	}
 }
