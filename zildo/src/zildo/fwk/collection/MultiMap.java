4e4dfb47f8442fbfac0dffbb4bb500a8aadbd20d
Evariste Boussaton <tchegito@gmail.com>
2017-01-02 21:35:55 +0100

    Gradle integration:
    -few changes to fit with Gradle requirements:
     *resources
     *compilation via JAVAC instead of Eclipse compiler

--- zildo/src/zildo/fwk/collection/MultiMap.java
+++ zildo/src/zildo/fwk/collection/MultiMap.java
@@ -4,22 +4,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@SuppressWarnings("serial")
-public class MultiMap<K, V> extends HashMap<K, List<V>> {
+public class MultiMap<K, V> {
 
-    @Override
+	Map<K, List<V>> map = new HashMap<K, List<V>>();
+	
     public List<V> put(K p_key, List<V> p_value) {
-	List<V> elems = get(p_key);
+    	List<V> elems = map.get(p_key);
         if (elems == null) {
             elems = new ArrayList<V>();
         }
         elems.addAll(p_value);
-        return super.put(p_key, elems);
+        return map.put(p_key, elems);
     }
     
     public List<V> put(K p_key, V p_value) {
-	return put(p_key, Collections.singletonList(p_value));
+    	return put(p_key, Collections.singletonList(p_value));
+    }
+    
+    public List<V> get(K p_key) {
+    	return map.get(p_key);
     }
     
 }
