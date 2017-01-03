574051f72319080421a93966d2d1ec0469736d4f
Evariste Boussaton <tchegito@gmail.com>
2017-01-03 16:37:43 +0100

    test

--- zildo-test/src/main/java/tools/EngineUT.java
+++ zildo-test/src/main/java/tools/EngineUT.java
@@ -29,6 +29,7 @@ import static org.mockito.Mockito.when;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -228,6 +229,7 @@ public class EngineUT {
 	public void setUp() {
 		// Check if environment variable is provided to get data folder
 		String dataFolder = System.getProperty("ZILDO_DATA");
+		Logger.getLogger(EngineUT.class.getSimpleName()).info("On a comme variable d'environnement ZILDO_DATA => "+dataFolder);
 		if (dataFolder != null) {
 			if (!dataFolder.endsWith(File.separator)) {
 				dataFolder += File.separator;
