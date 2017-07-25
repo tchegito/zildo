b4129dd641f86408dc9b234572ca14488725068c
Evariste Boussaton <tchegito@gmail.com>
2017-07-25 12:14:05 +0200

    bugfix about nails removal in forest

--- zildo-test/src/test/java/junit/area/ScenesBugFixes.java
+++ zildo-test/src/test/java/junit/area/ScenesBugFixes.java
@@ -3,9 +3,9 @@ package junit.area;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import tools.EngineUT;
@@ -23,5 +23,22 @@ public class ScenesBugFixes extends EngineUT {
 		waitEndOfScripting();
 		// Ruben noticed sound was played each time => that was a bug !
 		verify(EngineZildo.soundManagement, never()).playSound(eq(BankSound.ZildoSecret), any(PersoPlayer.class), eq(false));
+	}
+	
+	@Test
+	public void nailsInForest() {
+		mapUtils.loadMap("sousbois3");
+		int nails = 1277;
+		// Check that nails are in place
+		Assert.assertEquals(nails, mapUtils.area.readmap(11, 28));
+		EngineZildo.scriptManagement.accomplishQuest("removeNailForMoonStone", true);
+		waitEndOfScripting();
+		// After button is presed, check that nails are removed
+		Assert.assertEquals(54, mapUtils.area.readmap(11, 28));
+		
+		// Reload map, then check again
+		mapUtils.loadMap("sousbois3");
+		waitEndOfScripting();
+		Assert.assertEquals("Nails should have been removed !", 54, mapUtils.area.readmap(11, 28));
 	}
 }
