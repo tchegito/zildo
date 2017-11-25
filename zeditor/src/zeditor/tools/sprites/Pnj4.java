bcf2a5c94bc0c4596c3928782ef1f842c0831908
Evariste Boussaton <tchegito@gmail.com>
2017-11-25 15:05:10 +0100

    add bitey

--- zeditor/src/zeditor/tools/sprites/Pnj4.java
+++ zeditor/src/zeditor/tools/sprites/Pnj4.java
@@ -9,7 +9,7 @@ public class Pnj4 extends SpriteBanque {
 
 	public Pnj4() {
 		zones=new Zone[] {
-			/* Turret under floor (19)*/
+			/* Turret under floor */
 			new Zone(1, 1, 32, 25),
 			// Sub-heads + canon
 			new Zone(35, 5, 32, 21),new Zone(80, 5, 11, 5),
@@ -19,34 +19,34 @@ public class Pnj4 extends SpriteBanque {
 			new Zone(2,45,19,12), new Zone(1,28,15,15), 
 			// Sleeping king (8)
 			new Zone(0, 474, 18, 20), new Zone(19, 474, 18, 20), new Zone(38, 474, 18, 20),
-			// Druid
+			// Druid (11)
 			new Zone(69, 469, 16, 27), new Zone(86, 470, 14, 26),
 			new Zone(101, 469, 13, 27), new Zone(115, 470, 16, 26),
 			new Zone(113, 189, 16, 26), new Zone(130, 189, 16, 26),
-			// Bitey
-			/*
-		    new Zone(0, 3, 21, 24), new Zone(25, 5, 21, 22), new Zone(50, 5, 21, 22),
-		    new Zone(75, 3, 21, 24), new Zone(100, 2, 21, 25), new Zone(125, 1, 21, 26),
-		    new Zone(150, 0, 21, 27),
-		    */
-			// Vacto
+
+			// Vacto (17)
 			new Zone(0, 3, 16, 26), new Zone(16, 2, 16, 27),
 			new Zone(33, 4, 13, 25), new Zone(48, 3, 13, 26),
 			new Zone(64, 3, 14, 26), new Zone(79, 2, 16, 27),
 			
-		    // Dying squirrel
+		    // Dying squirrel (23)
 		    new Zone(1, 48, 14, 12),
 		    new Zone(17, 48, 14, 12), 
 		    new Zone(32, 47, 14, 13),
 		    new Zone(48, 47, 14, 13), new Zone(64, 49, 18, 11), new Zone(84, 54, 24, 6),
 		    
-		    // Fire elemental
+		    // Fire elemental (29)
 		    new Zone(167, 201, 23, 27), new Zone(193, 201, 23, 27),
 		    new Zone(218, 201, 23, 27), new Zone(242, 201, 25, 27),
 		    
-		    // Coal
+		    // Coal (33)
 		    new Zone(0, 372, 26, 24), new Zone(27, 372, 26, 24),
-		    new Zone(54, 377, 31, 19), new Zone(86, 383, 31, 13)
+		    new Zone(54, 377, 31, 19), new Zone(86, 383, 31, 13),
+			
+			// Bitey (37)
+		    new Zone(0, 3, 21, 24), new Zone(25, 5, 21, 22), new Zone(50, 5, 21, 22),
+		    new Zone(75, 3, 21, 24), new Zone(100, 2, 21, 25), new Zone(125, 1, 21, 26),
+		    new Zone(150, 0, 21, 27),
 
 		};
 		
@@ -54,6 +54,7 @@ public class Pnj4 extends SpriteBanque {
 				new GraphChange("pnj2", 8, 0),
 				new GraphChange("squirrel", 23, 0),
 				new GraphChange("dragonpal", 29, 0, true),
+				new GraphChange("bitey", 37, 0)
 		});
 	}
 }
